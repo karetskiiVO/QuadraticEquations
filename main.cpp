7c7960d4a72a0c3f0a3e02b2cca88376dba2adb4
s7kla4 <kar.vladi@yandex.ru>
2022-08-17 16:06:06 +0300

    v0.01
    new definitions of constants have been introduced

--- main.cpp
+++ main.cpp
@@ -5,14 +5,19 @@
 
 const double allowance = 1e-9; //delta neighborhood size
 
+enum exitStatus{
+    ERROR_IN_DEFINITION = -2,
+    INFINITE_NUMOF_SOLUTIONS = -1,
+    NO_SOLUTIONS = 0,
+    ONE_SOLUTION = 1,
+    TWO_SOLUTIONS = 2
+};
+
+
 void input(double* a, double* b, double* c);//input coefficients
 void output(double a, double b, double c);
 bool isEqual(double a, double b);
 int solveEq(double a, double b, double c, double* x);//get solutions
-/*return -2 == error in definition
-**return -1 == infinite number of solutions
-**if N diffrent
-**return N == number of solutions**/
 double getDisc(double a, double b, double c);//get discriminant
 
 int main(void){
@@ -34,17 +39,17 @@ void output(double a, double b, double c){
     double x[2];// x - array of solution
     int answer = solveEq(a, b, c, x);
 
-    if(answer == -2){
+    if(answer == ERROR_IN_DEFINITION){
         printf("error in definition");
         return;
     }
-    if(answer == -1){
+    if(answer == INFINITE_NUMOF_SOLUTIONS){
         printf("infinite number of solutions");
         return;
     }
     printf("Equation have %d solution", answer);
 
-    if(answer == 0){
+    if(answer == NO_SOLUTIONS){
         printf(".");
     }else{
         printf(":");
@@ -68,23 +73,23 @@ int solveEq(double a, double b, double c, double* x){
     if(isEqual(a, 0)){
         if(isEqual(b, 0)){
             if(!isEqual(c, 0)){
-                return 0;
+                return NO_SOLUTIONS;
             }
-            return -1;
+            return INFINITE_NUMOF_SOLUTIONS;
         }
         x[0] = - c / b;
-        return 1;
+        return ONE_SOLUTION;
     }
     D = getDisc(a, b, c);
     if(isEqual(D, 0)){
         x[0] = - b / (2 * a);
-        return 1;
+        return ONE_SOLUTION;
     }
     if(D < 0){
-        return 0;
+        return NO_SOLUTIONS;
     }
 
     x[0] = -(b + sqrt(D)) / (2 * a);
     x[1] = -(b - sqrt(D)) / (2 * a);
-    return 2;
+    return TWO_SOLUTIONS;
 }
